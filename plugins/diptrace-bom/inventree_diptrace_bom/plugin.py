5f9c8de56fe05d2e39e2985c52f2da44d55c0ef7
Amrit Niure <niureamrit2@gmail.com>
2026-09-21 13:05:50 +0930

    Show full JLC part identifiers

--- plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
+++ plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
@@ -45,7 +45,7 @@ class DipTraceBomPlugin(
     SLUG = "diptrace-bom"
     TITLE = "DipTrace BOM"
     DESCRIPTION = "Import DipTrace BOMs and synchronize InvenTree / JLCPCB availability"
-    VERSION = "0.3.23"
+    VERSION = "0.3.24"
     AUTHOR = "Corrosion Instruments"
     MIN_VERSION = "1.5.2"
 
