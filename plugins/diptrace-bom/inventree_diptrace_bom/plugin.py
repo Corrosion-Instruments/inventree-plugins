969e1a4fe768b4b9075510778c9306bd1d8ce0ab
Amrit Niure <niureamrit2@gmail.com>
2026-09-21 12:44:05 +0930

    Reorder BOM review columns

--- plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
+++ plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
@@ -45,7 +45,7 @@ class DipTraceBomPlugin(
     SLUG = "diptrace-bom"
     TITLE = "DipTrace BOM"
     DESCRIPTION = "Import DipTrace BOMs and synchronize InvenTree / JLCPCB availability"
-    VERSION = "0.3.19"
+    VERSION = "0.3.20"
     AUTHOR = "Corrosion Instruments"
     MIN_VERSION = "1.5.2"
 
