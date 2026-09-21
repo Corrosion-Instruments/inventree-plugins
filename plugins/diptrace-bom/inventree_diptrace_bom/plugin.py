e3aa90e1d0247642c84c56a9f382c5cacb913aca
Amrit Niure <niureamrit2@gmail.com>
2026-09-21 11:13:40 +0930

    Add external build capacity

--- plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
+++ plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
@@ -45,7 +45,7 @@ class DipTraceBomPlugin(
     SLUG = "diptrace-bom"
     TITLE = "DipTrace BOM"
     DESCRIPTION = "Import DipTrace BOMs and synchronize InvenTree / JLCPCB availability"
-    VERSION = "0.3.14"
+    VERSION = "0.3.15"
     AUTHOR = "Corrosion Instruments"
     MIN_VERSION = "1.5.2"
 
