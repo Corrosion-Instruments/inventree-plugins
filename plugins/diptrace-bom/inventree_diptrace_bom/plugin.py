88488b49f470a7a7392fe2995d5214a22f85101e
Amrit Niure <niureamrit2@gmail.com>
2026-09-21 11:09:15 +0930

    Combine JLC private inventory columns

--- plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
+++ plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
@@ -45,7 +45,7 @@ class DipTraceBomPlugin(
     SLUG = "diptrace-bom"
     TITLE = "DipTrace BOM"
     DESCRIPTION = "Import DipTrace BOMs and synchronize InvenTree / JLCPCB availability"
-    VERSION = "0.3.13"
+    VERSION = "0.3.14"
     AUTHOR = "Corrosion Instruments"
     MIN_VERSION = "1.5.2"
 
