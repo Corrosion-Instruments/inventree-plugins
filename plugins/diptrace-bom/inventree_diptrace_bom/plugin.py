86231fa4906ede23a5d46332d40b9478d6e97945
Amrit Niure <niureamrit2@gmail.com>
2026-09-21 10:23:09 +0930

    Unify plugin controls in page header

--- plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
+++ plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
@@ -45,7 +45,7 @@ class DipTraceBomPlugin(
     SLUG = "diptrace-bom"
     TITLE = "DipTrace BOM"
     DESCRIPTION = "Import DipTrace BOMs and synchronize InvenTree / JLCPCB availability"
-    VERSION = "0.3.2"
+    VERSION = "0.3.3"
     AUTHOR = "Corrosion Instruments"
     MIN_VERSION = "1.5.2"
 
