38ff8b04ded000cbe09bdfd588a74b973c3108cc
Amrit Niure <niureamrit2@gmail.com>
2026-09-21 13:13:44 +0930

    Restore exclusion count summary

--- plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
+++ plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
@@ -45,7 +45,7 @@ class DipTraceBomPlugin(
     SLUG = "diptrace-bom"
     TITLE = "DipTrace BOM"
     DESCRIPTION = "Import DipTrace BOMs and synchronize InvenTree / JLCPCB availability"
-    VERSION = "0.3.25"
+    VERSION = "0.3.26"
     AUTHOR = "Corrosion Instruments"
     MIN_VERSION = "1.5.2"
 
