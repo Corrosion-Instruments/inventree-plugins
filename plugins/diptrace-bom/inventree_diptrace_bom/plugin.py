cddad8d739ba207611ae5ba9a211d688f49cf347
Amrit Niure <niureamrit2@gmail.com>
2026-09-25 15:06:02 +0930

    Align catalogue preview action with file input

--- plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
+++ plugins/diptrace-bom/inventree_diptrace_bom/plugin.py
@@ -46,7 +46,7 @@ class DipTraceBomPlugin(
     SLUG = "diptrace-bom"
     TITLE = "DipTrace BOM"
     DESCRIPTION = "Import DipTrace BOMs and synchronize InvenTree / JLCPCB availability"
-    VERSION = "0.6.1"
+    VERSION = "0.6.2"
     AUTHOR = "Corrosion Instruments"
     MIN_VERSION = "1.5.2"
 
